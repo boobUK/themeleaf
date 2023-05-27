ecc391da6319e48ba164f432f4a3907701d516c5
boobUK <valentyn.ushych@outlook.com>
2023-05-27 12:26:15 +0300

    AllFiles

---
+++ themeleafTets/src/main/java/com/boobuk/themeleafTets/ThemeleafTetsApplication.java
@@ -0,0 +1,13 @@
+package com.boobuk.themeleafTets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeleafTetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeleafTetsApplication.class, args);
+	}
+
+}
---
+++ themeleafTets/src/test/java/com/boobuk/themeleafTets/ThemeleafTetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boobuk.themeleafTets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeleafTetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
