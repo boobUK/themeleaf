f4bd009602614fe0c50615866c79ebee337c01f1
valen <valen@192.168.88.30>
2023-05-27 22:33:38 +0300

    used form request

--- themeleafTets/src/main/java/com/boobuk/themeleafTets/controller/StudentPageController.java
+++ themeleafTets/src/main/java/com/boobuk/themeleafTets/controller/StudentPageController.java
@@ -3,6 +3,7 @@ package com.boobuk.themeleafTets.controller;
 import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,9 +31,26 @@ public class StudentPageController {
 
 		Student student2 = new Student();
 		student2.setName("Sam");
-		student2.setScore(50);
+		student2.setScore(25);
 
 		model.addObject("students", Arrays.asList(student1, student2));
 		return model;
 	}
+	
+	@RequestMapping("/studentForm")
+	public ModelAndView displayStudentForm() {
+		ModelAndView model = new ModelAndView("studentForm");
+		Student student = new Student();
+		model.addObject("student", student);
+		return model;
+	}
+	
+	@RequestMapping("/saveStudent")
+	public ModelAndView saveStudent(@ModelAttribute Student student) {
+		ModelAndView model = new ModelAndView("result");
+		System.out.println(student.getName());
+		System.out.println(student.getScore());
+		return model;
+	}
+
 }
